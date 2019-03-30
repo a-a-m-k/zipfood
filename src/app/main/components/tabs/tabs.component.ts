84dbf755a8e1ea345d1f1d1bc7596d500b549052
chorny14@gmail.com <chorny14@gmail.com>
2019-03-30 15:15:22 +0200

    added google map

--- src/app/main/components/tabs/tabs.component.ts
+++ src/app/main/components/tabs/tabs.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {BrowserModule, BrowserTransferStateModule, DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-tabs',
@@ -6,6 +7,15 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./tabs.component.css']
 })
 
-export class TabsComponent {
+export class TabsComponent implements OnInit {
   @Input() patient;
+  public url: any;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit(): void {
+    let aloeurl = `https://maps.google.com/maps?q=${this.patient.city}+${this.patient.street}+${this.patient.house}&t=&z=13&ie=UTF8&iwloc=&output=embed`;
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(aloeurl);
+  }
+
 }
